1b73a4597877f1d9f2755c9a0cf4c0c4017e5e64
Deathstroke1096 <sayantan.sandy@gmail.com>
2022-08-08 01:47:57 +0530

    save

--- createStack.sh
+++ createStack.sh
@@ -1,5 +1,5 @@
 echo -e "Creating Stack ....."
-aws cloudformation create-stack --stack-name blue-green --template-body file://blueGreen.yaml --parameters file://param.json --region us-east-1
-echo -e "Stack will be created in few moments...\n"
-aws cloudformation wait stack-create-complete --stack-name blue-green
-echo -e "Stack Created Successfully !!! \n"
+aws cloudformation create-stack --stack-name blue-green --template-body file://blueGreen.yaml --parameters file://param.json
+#echo -e "Stack will be created in few moments...\n"
+#aws cloudformation wait stack-create-complete --stack-name blue-green
+#echo -e "Stack Created Successfully !!! \n"
