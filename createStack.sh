575b858ad8aa8f239840b4824f39ce769d1d6574
Deathstroke1096 <sayantan.sandy@gmail.com>
2022-08-07 22:00:24 +0530

    stack final

--- createStack.sh
+++ createStack.sh
@@ -1,8 +1,5 @@
 echo -e "Creating Stack ....."
-
-
 aws cloudformation create-stack --stack-name blue-green --template-body file://blueGreen.yaml --parameters file://param.json
 echo -e "Stack will be created in few moments...\n"
 aws cloudformation wait stack-create-complete --stack-name blue-green
-
 echo -e "Stack Created Successfully !!! \n"
