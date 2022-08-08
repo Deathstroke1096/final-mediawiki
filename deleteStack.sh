ecc924fd50c8601b0001731c74abeb0144920de5
Deathstroke1096 <sayantan.sandy@gmail.com>
2022-08-09 01:26:26 +0530

    deleteFinal

--- deleteStack.sh
+++ deleteStack.sh
@@ -1,3 +1,7 @@
+export AWS_ACCESS_KEY_ID=AKIAQC7RXWCSBAEJKS7H && \
+export AWS_SECRET_ACCESS_KEY=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 &&\
+export DEFAULT_REGION_NAME=us-east-1 &&\
+export DEFAULT_OUTPUT_FORMAT=json
 echo -e "Deleting the Stack... "
 aws cloudformation delete-stack --stack-name blue-green
 aws cloudformation wait stack-delete-complete --stack-name blue-green
