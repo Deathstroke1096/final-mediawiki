03928cde87b6cc3a5900d6794815830c2fbf0368
Deathstroke1096 <sayantan.sandy@gmail.com>
2022-08-08 17:30:53 +0530

    final

--- deleteStack.sh
+++ deleteStack.sh
@@ -1,4 +1,4 @@
 echo -e "Deleting the Stack... "
-aws cloudformation delete-stack --stack-name blue-green --region us-east-1
+aws cloudformation delete-stack --stack-name blue-green --region
 aws cloudformation wait stack-delete-complete --stack-name blue-green
 echo "Stacks Deleted along with RDS and EC2 instances..." 
